68b2b7e4a07de44a54b94c22cf74ee02c6e93b6c
Cristian <zahir.r.2568@gmail.com>
2023-08-02 16:53:55 -0600

    Inicial

---
+++ Linterna2/Linterna2App.swift
@@ -0,0 +1,17 @@
+//
+//  Linterna2App.swift
+//  Linterna2
+//
+//  Created by iOS Development Lab FI UNAM on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Linterna2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
